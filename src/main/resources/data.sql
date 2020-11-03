6ccbbf5eab716579afbdc080131a58a8832e7e65
chipo <pnapoli33@gmail.com>
2020-11-03 20:10:08 -0300

    Add a script with insersts for Department and Employee tables.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO department VALUES ('1', '150');
+INSERT INTO department VALUES ('2', '390');
+INSERT INTO department VALUES ('3', '103');
+
+INSERT INTO employee VALUES (1, 'john connor', 'dev', '120000', '2');
+INSERT INTO employee VALUES (2, 'luis ramirez', 'dev', '125000', '2');
+INSERT INTO employee VALUES (3, 'jose lopez', 'mng', '170000', '2');
+INSERT INTO employee VALUES (4, 'tom lee', 'mng', '155000', '1');
+INSERT INTO employee VALUES (5, 'ivan drago', 'dev', '120000', '1');
