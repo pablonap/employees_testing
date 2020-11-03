6773f489b9ea6d5a4a9d44ab316b749315620b7c
chipo <pnapoli33@gmail.com>
2020-11-03 19:35:17 -0300

    Initial commit

---
+++ src/main/java/com/binary_winters/employees_testing/EmployeesTestingApplication.java
@@ -0,0 +1,13 @@
+package com.binary_winters.employees_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binary_winters/employees_testing/EmployeesTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary_winters.employees_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
