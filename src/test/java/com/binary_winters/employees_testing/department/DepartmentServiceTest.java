01f89f1e2180a1a17bfa608be3ea97ff08830a8d
chipo <pnapoli33@gmail.com>
2020-11-04 01:25:54 -0300

    Create DepartmentServiceTest

--- src/test/java/com/binary_winters/employees_testing/department/DepartmentServiceTest.java
+++ src/test/java/com/binary_winters/employees_testing/department/DepartmentServiceTest.java
@@ -1,10 +1,10 @@
-package com.binary_winters.employees_testing;
+package com.binary_winters.employees_testing.department;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeesTestingApplicationTests {
+class DepartmentServiceTest {
 
 	@Test
 	void contextLoads() {
