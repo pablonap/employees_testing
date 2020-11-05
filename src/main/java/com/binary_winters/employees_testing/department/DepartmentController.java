e543aaef71c634bf55247f0d4e6b46e723224614
chipo <pnapoli33@gmail.com>
2020-11-05 01:34:36 -0300

    Change GET HTTP method to PUT.

--- src/main/java/com/binary_winters/employees_testing/department/DepartmentController.java
+++ src/main/java/com/binary_winters/employees_testing/department/DepartmentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class DepartmentController {
 		return departmentService.getAllDepartment();
 	}
 	
-	@GetMapping("/departments/assign-plus")
+	@PutMapping("/departments/assign-plus")
 	public GenericResponse assignPlus() {
 		String plusMessage = departmentService.assignPlusToEmployees();
 
